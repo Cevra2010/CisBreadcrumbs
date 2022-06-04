4224b585b9be9dd0d235d5a2c11069163d24d81d
Markus Klebsattel <markusklebsattel@gmail.com>
2022-06-04 10:35:27 +0200

    documentation

--- src/Breadcrumb.php
+++ src/Breadcrumb.php
@@ -1,20 +1,57 @@
 <?php
 namespace CisFoundation\Breadcrumbs;
 
+/**
+ * The Breadcrumb-Class is the Object of every single breadcrumb object
+ */
 class Breadcrumb {
 
-
+    /**
+     * Slug of the breadcumb object
+     *
+     * @var string
+     */
     public $slug;
+
+    /**
+     * Slug of the parent Breadcumb-Object
+     *
+     * @var string
+     */
     public $parentSlug;
+
+    /**
+     * Laravel route
+     *
+     * @var string
+     */
     public $route;
+
+    /**
+     * Route parameters
+     *
+     * @var array
+     */
     public $routeParameter;
+
+    /**
+     * Title of the Breadcrumb
+     *
+     * @var string
+     */
     public $title;
+
 
     public function __construct()
     {
 
     }
 
+    /**
+     * Retuns true if this objects instance has an parent Breadcumb. Otherwise return false.
+     *
+     * @return boolean
+     */
     public function hasParent() {
         if($this->parentSlug) {
             return true;
--- src/Breadcrumbs.php
+++ src/Breadcrumbs.php
@@ -2,19 +2,57 @@
 namespace CisFoundation\Breadcrumbs;
 
 use CisFoundation\Breadcrumbs\Exception\BreadcrumbNotFoundException;
-use Exception;
+use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Config;
 
+/**
+ * Breadcrumbs for Cis-Foundation manages the existing breadcrumbs for the
+ * views of each page the base and extended cis backend modules
+ */
 class Breadcrumbs {
 
     protected static $breadcrumbsColleciton = null;
+    protected static $registeredBreadcrumbs = null;
 
+    /**
+     * initializing the breadcrumb instance
+     *
+     * @return void
+     */
     public static function boot() {
+
+        /** Register application base breadcrumbs */
+        if(!self::$registeredBreadcrumbs === null)
+        {
+            self::$registeredBreadcrumbs[] = Config::get('breadcrumbs');
+        }
+    }
+
+    /**
+     * Register new breadcrumbs
+     *
+     * @param array $breadcrumbs
+     * @return void
+     */
+    public static function registerBreadcrumbs($breadcrumbs) {
+        self::$registeredBreadcrumbs[] = $breadcrumbs;
+    }
+
+    /**
+     * Returns a related colleciton of breadcrumb objects.
+     * Starts with the first registed element.
+     *
+     * @param string $searchedSlug
+     * @return Collection
+     */
+    public static function get($searchedSlug) {
+
         if(self::$breadcrumbsColleciton === null)
         {
             self::$breadcrumbsColleciton = collect();
         }
 
+        /** Read the configuration */
         foreach(Config::get('breadcrumbs') as $breadcrumb) {
             $breadcrumbObject = new Breadcrumb();
             $breadcrumbObject->slug = (isset($breadcrumb['slug']) ? $breadcrumb['slug'] : null);
@@ -23,21 +61,21 @@ class Breadcrumbs {
             $breadcrumbObject->route = (isset($breadcrumb['route']) ? $breadcrumb['route'] : null);
             self::$breadcrumbsColleciton->add($breadcrumbObject);
         }
-    }
 
-    public static function get($searchedSlug) {
         $breadcrumbs = collect();
         if($crumb = self::$breadcrumbsColleciton->where('slug',$searchedSlug)->first()) {
             $breadcrumbs->add($crumb);
         }
 
-        while($crumb->hasParent()) {
-            $parentSlug = $crumb->parentSlug;
-            if($crumb = self::$breadcrumbsColleciton->where('slug',$parentSlug)->first()) {
-                $breadcrumbs->add($crumb);
-            }
-            else {
-                throw new BreadcrumbNotFoundException($parentSlug);
+        if($crumb) {
+            while($crumb->hasParent()) {
+                $parentSlug = $crumb->parentSlug;
+                if($crumb = self::$breadcrumbsColleciton->where('slug',$parentSlug)->first()) {
+                    $breadcrumbs->add($crumb);
+                }
+                else {
+                    throw new BreadcrumbNotFoundException($parentSlug);
+                }
             }
         }
 
